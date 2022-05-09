3b49bfc77547334f6807443cd015118907beceff
Liam <liam@scalzulli.com>
2022-05-09 13:45:59 -0400

    Format

--- src/main.js
+++ src/main.js
@@ -35,31 +35,31 @@ class Computer {
       throw 'Failed to initialize WebGL context';
     }
 
-    this.program = this.createProgram(
-      [{'code': vert, 'type': 'vertex'},
-       {'code': frag, 'type': 'fragment'}]
-    );
+    this.program = this.createProgram([
+      { code: vert, type: 'vertex' },
+      { code: frag, type: 'fragment' },
+    ]);
 
     this.length = this.setupTriangles();
     this.source = 0;
     this.textures = new Array(2).fill(null).map(() => this.createTexture());
     this.textureLocation = this.gl.getUniformLocation(this.program, 'source');
+    this.gl.uniform1i(this.textureLocation, 0);
     this.frameBuffer = this.gl.createFramebuffer();
   }
 
   run(e) {
-    const gl = this.gl;
-
     const input = e.target.value;
 
-    input
-    .split(' ')
-    .forEach((token) => {
-      console.log(token);
+    input.split(' ').forEach((token) => {
       switch (token) {
         case 'apply':
-          // TODO: apply a selected mask, default to invert
-          this.apply(() => gl.uniform1i(gl.getUniformLocation(this.program, 'invert'), 1));
+          this.apply(() =>
+            this.gl.uniform1i(
+              this.gl.getUniformLocation(this.program, 'invert'),
+              1
+            )
+          );
           break;
         default:
           throw `Failed to compile program: ${input[0]}`;
@@ -73,7 +73,15 @@ class Computer {
     const gl = this.gl;
 
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer);
-    gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, this.textures[this.source ^ 1], 0);
+
+    gl.framebufferTexture2D(
+      gl.FRAMEBUFFER,
+      gl.COLOR_ATTACHMENT0,
+      gl.TEXTURE_2D,
+      this.textures[this.source ^ 1],
+      0
+    );
+
     gl.bindTexture(gl.TEXTURE_2D, this.textures[this.source]);
     mask();
     gl.drawArrays(gl.TRIANGLES, 0, this.length);
@@ -85,9 +93,7 @@ class Computer {
     const gl = this.gl;
 
     const shader = gl.createShader(
-      item.type === 'vertex' ?
-      gl.VERTEX_SHADER :
-      gl.FRAGMENT_SHADER
+      item.type === 'vertex' ? gl.VERTEX_SHADER : gl.FRAGMENT_SHADER
     );
 
     gl.shaderSource(shader, item.code.trim());
@@ -106,7 +112,19 @@ class Computer {
     const texture = gl.createTexture();
 
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 256, 256, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
+
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.RGBA,
+      256,
+      256,
+      0,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      null
+    );
+
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
@@ -122,8 +140,8 @@ class Computer {
     code
       .map((item) => this.createShader(item))
       .forEach((shader) => {
-      gl.attachShader(program, shader);
-    });
+        gl.attachShader(program, shader);
+      });
 
     gl.linkProgram(program);
 
@@ -134,7 +152,7 @@ class Computer {
     gl.useProgram(program);
 
     return program;
-  };
+  }
 
   drawToCanvas() {
     const gl = this.gl;
@@ -148,7 +166,17 @@ class Computer {
   setupTriangles() {
     const gl = this.gl;
 
-    const vertices = new Float32Array([-1, -1, 0, 1, -1, 0, 1, 1, 0, 1, 1, 0, -1, 1, 0, -1, -1, 0]);
+    const vertices = new Float32Array(
+      [
+        [-1, -1, 0],
+        [1, -1, 0],
+        [1, 1, 0],
+        [1, 1, 0],
+        [-1, 1, 0],
+        [-1, -1, 0],
+      ].flat()
+    );
+
     const position = gl.getAttribLocation(this.program, 'position');
 
     gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
@@ -163,10 +191,12 @@ class Computer {
 const main = () => {
   try {
     const computer = new Computer();
-    document.getElementById('program').addEventListener('input', (e) => computer.run(e));
+    document
+      .getElementById('program')
+      .addEventListener('input', (e) => computer.run(e));
   } catch (error) {
     console.error(`error: ${error}`);
   }
-}
+};
 
 main();
