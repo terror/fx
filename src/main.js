73aa81822797a02ea30508c474341a2b5b037a9c
Liam <liam@scalzulli.com>
2022-05-13 10:55:41 -0400

    Dedent filter functions

--- src/main.js
+++ src/main.js
@@ -62,40 +62,40 @@ vec2 barrel(vec2 coord, float amt) {
 }
 
 float sat(float t) {
-	return clamp(t, 0.0, 1.0);
+  return clamp(t, 0.0, 1.0);
 }
 
 float linterp(float t) {
-	return sat(1.0 - abs(2.0 * t - 1.0));
+  return sat(1.0 - abs(2.0 * t - 1.0));
 }
 
 float remap(float t, float a, float b) {
-	return sat((t - a) / (b - a));
+  return sat((t - a) / (b - a));
 }
 
-const float max_distort     = 2.2;
-const int num_iter          = 12;
+const float max_distort = 2.2;
+const int num_iter = 12;
 const float reci_num_iter_f = 1.0 / float(num_iter);
 
 vec3 spectrum_offset(float t) {
-	vec3 ret;
-	float lo = step(t, 0.5);
-	float hi = 1.0 - lo;
-	float w = linterp(remap(t, 1.0 / 6.0, 5.0 / 6.0));
-	ret = vec3(lo, 1.0, hi) * vec3(1.0 - w, w, 1.0 - w);
-	return pow(ret, vec3(1.0 / 2.2));
+  vec3 ret;
+  float lo = step(t, 0.5);
+  float hi = 1.0 - lo;
+  float w = linterp(remap(t, 1.0 / 6.0, 5.0 / 6.0));
+  ret = vec3(lo, 1.0, hi) * vec3(1.0 - w, w, 1.0 - w);
+  return pow(ret, vec3(1.0 / 2.2));
 }
 
 vec4 _aberrate() {
-	vec3 sumcol = vec3(0.0);
-	vec3 sumw = vec3(0.0);
+  vec3 sumcol = vec3(0.0);
+  vec3 sumw = vec3(0.0);
 
-	for (int i = 0; i < num_iter; ++i) {
-		float t = float(i) * reci_num_iter_f;
-		vec3 w = spectrum_offset(t);
-		sumw += w;
-		sumcol += w * texture(source, barrel(uv, max_distort * t)).rgb;
-	}
+  for (int i = 0; i < num_iter; ++i) {
+    float t = float(i) * reci_num_iter_f;
+    vec3 w = spectrum_offset(t);
+    sumw += w;
+    sumcol += w * texture(source, barrel(uv, max_distort * t)).rgb;
+  }
 
   return vec4(sumcol.rgb / sumw, 1.0);
 }
